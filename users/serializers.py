60520bdfbeb8b6d26545cefff971c9cb60318888
helena <heju1958@outlook.com>
2022-12-13 14:05:25 -0300

    feat: tarefa 2 and tests

--- users/serializers.py
+++ users/serializers.py
@@ -4,11 +4,35 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message="A user with that username already exists.",
+            )
+        ]
+    )
+    email = serializers.EmailField(
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+            )
+        ]
+    )
+
     class Meta:
         model = User
-        fields = "__all__"
         extra_kwargs = {"password": {"write_only": True}}
-        read_only_fields = ["is_superuser"]
+        read_only_fields = ["id", "is_superuser"]
+        fields = [
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "is_superuser",
+            "password",
+        ]
 
     def create(self, validated_data: dict) -> User:
         return User.objects.create_superuser(**validated_data)
