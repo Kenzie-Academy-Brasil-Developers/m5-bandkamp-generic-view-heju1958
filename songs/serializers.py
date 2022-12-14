24f538a73afafa44bea17e92961f1b57eac76204
helena <heju1958@outlook.com>
2022-12-14 10:40:38 -0300

    feat: tarefa 3 song view

--- songs/serializers.py
+++ songs/serializers.py
@@ -6,8 +6,8 @@ from .models import Song
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
-        fields = "__all__"
-        read_only_fields = ["album_id"]
+        fields = ["id", "title", "duration", "album_id"]
+        read_only_fields = ["id", "album_id"]
 
     def create(self, validated_data):
         return Song.objects.create(**validated_data)
